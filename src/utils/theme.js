3f6346e80f8213682ff95783b7684de8711153e3
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-04-16 16:37:21 +0800

    add theme

---
+++ src/utils/theme.js
@@ -0,0 +1,104 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+let theme = createMuiTheme({
+	typography: {
+		useNextVariants: true,
+	},
+	palette: {
+		primary: {
+			light: '#33877c',
+			main: '#00695c',
+			dark: '#004940',
+		},
+		secondary: {
+			light: '#873363',
+			main: '#69003c',
+			dark: '#49002a',
+		},
+	},
+	shape: {
+		borderRadius: 8,
+	},
+});
+
+theme = {
+	...theme,
+	overrides: {
+		MuiButton: {
+			label: {
+				textTransform: 'initial',
+			},
+			contained: {
+				boxShadow: 'none',
+				'&:active': {
+					boxShadow: 'none',
+				},
+			},
+		},
+		MuiTabs: {
+			root: {
+				marginLeft: theme.spacing.unit,
+			},
+			indicator: {
+				height: 3,
+				borderTopLeftRadius: 3,
+				borderTopRightRadius: 3,
+				backgroundColor: theme.palette.common.white,
+			},
+		},
+		MuiTab: {
+			root: {
+				textTransform: 'initial',
+				margin: '0 16px',
+				minWidth: 0,
+				[theme.breakpoints.up('md')]: {
+					minWidth: 0,
+				},
+			},
+			labelContainer: {
+				padding: 0,
+				[theme.breakpoints.up('md')]: {
+					padding: 0,
+				},
+			},
+		},
+		MuiTooltip: {
+			tooltip: {
+				borderRadius: 4,
+			},
+		},
+		MuiDivider: {
+			root: {
+				backgroundColor: '#404854',
+			},
+		},
+		MuiTableCell: {
+			root: {
+				padding: 10,
+			},
+			head: {
+				padding: 0,
+				textAlign: 'center',
+				'&:last-child': {
+					padding: 0,
+				},
+			},
+		},
+		MuiAvatar: {
+			root: {
+				width: 32,
+				height: 32,
+			},
+		},
+	},
+	props: {
+		MuiTab: {
+			disableRipple: true,
+		},
+	},
+	mixins: {
+		...theme.mixins,
+	},
+};
+
+export default theme;

113a5fec72ac5827ecbfcb143bf9e44687e5f95a
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-04-16 20:15:26 +0800

    display confidentialities using table

--- src/panels/Confidentialities.js
+++ src/panels/Confidentialities.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
 
+import { fetchConfidentialities } from '../actions/confidentialities.actions';
 import Master from '../layouts/Master';
 
-const Confidentialities = () => {
+const Confidentialities = ({ confidentialities, fetchConfidentialities }) => {
+	useEffect(() => {
+		fetchConfidentialities();
+	}, []);
+	
+	const computeTotal = () => {
+		return confidentialities.map(({ total_docs }) => total_docs).reduce((sum, i) => sum + i, 0);
+	};
 	return (
 		<Master headerTitle='Confidentialities'>
-			Confidentialities
+			<Paper>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell>ID</TableCell>
+							<TableCell>Name</TableCell>
+							<TableCell>Total Docs</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{confidentialities.map(c => (
+							<TableRow key={c.id}>
+								<TableCell>{c.id}</TableCell>
+								<TableCell>{c.name}</TableCell>
+								<TableCell>{c.total_docs}</TableCell>
+							</TableRow>
+						))}
+						<TableRow>
+							<TableCell />
+							<TableCell>Total</TableCell>
+							<TableCell>{computeTotal()}</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</Paper>
 		</Master>
 	);
 };
 
-export default Confidentialities;
+const mapStateToProps = ({ confidentialities: { data } }) => {
+	return {
+		confidentialities: data,
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		fetchConfidentialities: () => dispatch(fetchConfidentialities()),
+	};
+};
+
+export default compose(
+	connect(mapStateToProps, mapDispatchToProps)
+)(Confidentialities);

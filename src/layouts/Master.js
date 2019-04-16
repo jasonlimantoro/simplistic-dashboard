c4ee3d1f0bceb2c411280c046ce38ee4303dc59f
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-04-16 17:14:52 +0800

    fix master's style

--- src/layouts/Master.js
+++ src/layouts/Master.js
@@ -1,18 +1,41 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { compose } from 'recompose';
+
+import { STYLES } from '../utils/constants';
 
 import Header from './Header';
 import Sidebar from './Sidebar';
 
-const Master = ({ children, headerTitle }) => {
+const styles = theme => ({
+	main: {
+		display: 'grid',
+		gridTemplateRows: `${STYLES.APPBAR_HEIGHT} auto`,
+		[theme.breakpoints.up('md')]: {
+			gridTemplateColumns: `${STYLES.SIDEBAR_WIDTH} auto`,
+		},
+	},
+	content: {
+		margin: '30px 15px',
+		[theme.breakpoints.up('md')]: {
+			gridColumnStart: 2,
+			gridRowStart: 2,
+		},
+	},
+});
+
+const Master = ({ children, headerTitle, classes }) => {
 	return (
-		<Fragment>
+		<main className={classes.main}>
 			<Header>
 				<Fragment key='header-title'>{headerTitle}</Fragment>
 				<Sidebar key='sidebar' />
 			</Header>
-			{children}
-		</Fragment>
+			<div className={classes.content}>
+				{children}
+			</div>
+		</main>
 	);
 };
 
@@ -24,4 +47,6 @@ Master.defaultProps = {
 	headerTitle: 'Some Title',
 };
 
-export default Master;
+export default compose(
+	withStyles(styles)
+)(Master);

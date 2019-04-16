c2342fa6504bd6992c25e13a2f06243d85fc527a
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-04-16 17:15:06 +0800

    define redux store

---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers';
+
+const configureStore = (preloadedState) => {
+	const middlewares = [thunkMiddleware];
+	const middlewareEnhancer = applyMiddleware(...middlewares);
+	
+	const enhancers = [middlewareEnhancer];
+	const composedEnhancers = composeWithDevTools(...enhancers);
+
+	return createStore(rootReducer, preloadedState, composedEnhancers);
+};
+
+export const preloadedState = {
+	confidentialities: {
+		data: [],
+		status: {},
+		response: {},
+		error: '',
+	},
+	doctypes: {
+		data: [],
+		status: {},
+		response: {},
+		error: '',
+	},
+	languages: {
+		data: [],
+		status: {},
+		response: {},
+		error: '',
+	},
+};
+
+const store = configureStore(preloadedState);
+
+export default store;

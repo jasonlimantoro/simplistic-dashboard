61daeb3be573155079b2186468bbee92f32f763f
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-09 18:12:53 +0800

    TEST(unit.reducers): add test for reducers

---
+++ src/tests/reducers/confidentialities.reducer.test.js
@@ -0,0 +1,89 @@
+import reducer from '../../reducers/confidentialities.reducer';
+
+const defaultState = {
+	api: {
+		data: [],
+		status: '',
+		response: {},
+		error: '',
+	},
+	filter: '',
+};
+
+describe('Confidentialities Reducer', () => {
+	it('should have a valid default state', () => {
+		const actual = reducer(undefined, { type: 'UNDEFINED' });
+		
+		expect(actual).toEqual(defaultState);
+	});
+	
+	describe('api key', () => {
+		it('can start fetching confidentialities', () => {
+			const actual = reducer(undefined, {
+				type: 'FETCH_CONFIDENTIALITIES_REQUEST',
+			});
+			
+			const expected = {
+				...defaultState.api,
+				status: 'loading',
+			};
+			
+			expect(actual.api).toEqual(expected);
+		});
+		
+		it('can fetch confidentialities', () => {
+			const fake = [
+				{
+					id: 1,
+					name: 'secret',
+				},
+				{
+					id: 2,
+					name: 'Top Secret',
+				}
+			];
+			
+			const expected = {
+				...defaultState.api,
+				data: fake,
+				status: 'success',
+			};
+			
+			const actual = reducer(undefined, {
+				type: 'FETCH_CONFIDENTIALITIES_SUCCESS',
+				confidentialities: fake,
+			});
+			
+			expect(actual.api).toEqual(expected);
+		});
+		
+		it('can handle fetch error', () => {
+			const error = { message: 'NETWORK ERROR' };
+			const actual = reducer(undefined, {
+				type: 'FETCH_CONFIDENTIALITIES_ERROR',
+				error,
+			});
+			
+			const expected = {
+				...defaultState.api,
+				status: 'error',
+				error,
+			};
+			
+			expect(actual.api).toEqual(expected);
+		});
+	});
+	
+	describe('filter key', () => {
+		it('can filter', () => {
+			const filter = 'secret';
+			const actual = reducer(undefined, {
+				type: 'SET_FILTER',
+				filter,
+			});
+			const expected = filter;
+			
+			expect(actual.filter).toEqual(expected);
+		});
+	});
+});
---
+++ src/tests/reducers/languages.reducer.test.js
@@ -0,0 +1,89 @@
+import reducer from '../../reducers/languages.reducer';
+
+const defaultState = {
+	api: {
+		data: [],
+		status: '',
+		response: {},
+		error: '',
+	},
+	filter: '',
+};
+
+describe('Languages Reducer', () => {
+	it('should have a valid default state', () => {
+		const actual = reducer(undefined, { type: 'UNDEFINED' });
+		
+		expect(actual).toEqual(defaultState);
+	});
+	
+	describe('api key', () => {
+		it('can start fetching confidentialities', () => {
+			const actual = reducer(undefined, {
+				type: 'FETCH_LANGUAGES_REQUEST',
+			});
+			
+			const expected = {
+				...defaultState.api,
+				status: 'loading',
+			};
+			
+			expect(actual.api).toEqual(expected);
+		});
+		
+		it('can fetch confidentialities', () => {
+			const fake = [
+				{
+					id: 1,
+					name: 'secret',
+				},
+				{
+					id: 2,
+					name: 'Top Secret',
+				}
+			];
+			
+			const expected = {
+				...defaultState.api,
+				data: fake,
+				status: 'success',
+			};
+			
+			const actual = reducer(undefined, {
+				type: 'FETCH_LANGUAGES_SUCCESS',
+				languages: fake,
+			});
+			
+			expect(actual.api).toEqual(expected);
+		});
+		
+		it('can handle fetch error', () => {
+			const error = { message: 'NETWORK ERROR' };
+			const actual = reducer(undefined, {
+				type: 'FETCH_LANGUAGES_ERROR',
+				error,
+			});
+			
+			const expected = {
+				...defaultState.api,
+				status: 'error',
+				error,
+			};
+			
+			expect(actual.api).toEqual(expected);
+		});
+	});
+	
+	describe('filter key', () => {
+		it('can filter', () => {
+			const filter = 'secret';
+			const actual = reducer(undefined, {
+				type: 'SET_FILTER',
+				filter,
+			});
+			const expected = filter;
+			
+			expect(actual.filter).toEqual(expected);
+		});
+	});
+});
